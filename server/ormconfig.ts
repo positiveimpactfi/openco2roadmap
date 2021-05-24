cfc53c2c8e6ee8ae9dddab3ba63d6861b977ea58
Yusif Salam-zade <yusalam@gmail.com>
2021-05-24 14:47:15 +0300

    set up connection to db in docker

---
+++ server/ormconfig.ts
@@ -0,0 +1,18 @@
+export default {
+  type: "postgres",
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  username: process.env.PG_USERNAME,
+  password: process.env.PG_PW,
+  database: process.env.PG_DB,
+  synchronize: true,
+  logging: false,
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscriber/**/*.ts"],
+  cli: {
+    entitiesDir: "src/entity",
+    migrationsDir: "src/migration",
+    subscribersDir: "src/subscriber",
+  },
+};
