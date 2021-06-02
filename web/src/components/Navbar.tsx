2d1004cd27f3b0ceb4fa8c0a9f1d427f6ea2b167
Yusif Salam-zade <yusalam@gmail.com>
2021-06-02 18:46:18 +0300

    frontend: user logout

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,21 +1,38 @@
-import { useMeQuery } from "generated/graphql";
+import { useApolloClient } from "@apollo/client";
+import { useLogoutMutation, useMeQuery } from "generated/graphql";
 import Link from "next/link";
 
 interface NavbarProps {}
 
 export const Navbar: React.FC<NavbarProps> = ({}) => {
-  const { data, loading } = useMeQuery();
+  const { data } = useMeQuery();
+  const [logout] = useLogoutMutation();
+  const apolloClient = useApolloClient();
+
+  const isLoggedIn = data?.me?.email !== undefined;
+
+  const handleLogout = async () => {
+    await logout();
+    await apolloClient.resetStore();
+  };
+
   return (
     <>
       <div className="flex flex-row justify-between w-full p-4">
         <div>{data?.me?.email ?? "Not logged in"}</div>
         <div className="flex flex-row gap-2 ml-auto">
-          <Link href="/login" passHref>
-            <a>Login</a>
-          </Link>
-          <Link href="/register" passHref>
-            <a>Register</a>
-          </Link>
+          {isLoggedIn ? (
+            <button onClick={handleLogout}>Logout</button>
+          ) : (
+            <>
+              <Link href="/login" passHref>
+                <a>Login</a>
+              </Link>
+              <Link href="/register" passHref>
+                <a>Register</a>
+              </Link>{" "}
+            </>
+          )}
         </div>
       </div>
     </>
