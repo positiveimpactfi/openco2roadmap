64116344e2e9e08384aab04b5ede96a426500441
Yusif Salam-zade <yusalam@gmail.com>
2021-06-02 15:05:47 +0300

    backend: set up cors and simple logging

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,46 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import Redis from "ioredis";
 import session from "express-session";
 import connectRedis from "connect-redis";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { UserResolver } from "./resolvers/user";
+import cors from "cors";
+
+const requestLogger = (
+  request: Request,
+  _response: Response,
+  next: NextFunction
+) => {
+  console.log("Method", request.method);
+  console.log("Path: ", request.path);
+  console.log("Body: ", request.body);
+  console.log("----");
+  next();
+};
 
 const main = async () => {
   await createConnection();
   const app = express();
+  app.use(express.json());
 
   const RedisStore = connectRedis(session);
   const redis = new Redis(process.env.REDIS_URL, {
     password: process.env.REDIS_PW,
   });
+
+  app.set("trust proxy", 1);
+  console.log("cors origin", process.env.CORS_ORIGIN);
+  app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+    })
+  );
+
+  app.use(requestLogger);
 
   app.use(
     session({
