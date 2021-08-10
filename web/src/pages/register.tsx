afe347a6b00d90df7a9f8d51081af3274db9e0c0
Yusif Salam-zade <yusalam@gmail.com>
2021-08-10 14:13:44 +0300

    frontend: redesign register page

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -19,51 +19,59 @@ const RegisterPage = () => {
   const [registerMutation] = useRegisterMutation();
   const router = useRouter();
   return (
-    <div>
-      Register
-      <br />
-      <Formik
-        initialValues={{ email: "", password: "" }}
-        validationSchema={RegisterSchema}
-        onSubmit={async (values, { setSubmitting, setErrors }) => {
-          const response = await registerMutation({ variables: values });
-          if (response.data?.register.errors) {
-            setErrors(errorArrayToObject(response.data.register.errors));
-          } else {
-            router.push("/");
-          }
-          setSubmitting(false);
-        }}
-      >
-        {({ isSubmitting }: FormikProps<RegisterFormProps>) => (
-          <Form>
-            <div className="mx-8 my-4 max-w-md w-full space-y-8">
-              <div className="flex flex-col justify-center items-center bg:white p-4 shadow rounded-lg space-y-6 ">
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validationSchema={RegisterSchema}
+      onSubmit={async (values, { setSubmitting, setErrors }) => {
+        const response = await registerMutation({ variables: values });
+        if (response.data?.register.errors) {
+          setErrors(errorArrayToObject(response.data.register.errors));
+        } else {
+          router.push("/");
+        }
+        setSubmitting(false);
+      }}
+    >
+      {({ isSubmitting }: FormikProps<RegisterFormProps>) => (
+        <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 w-screen">
+          <div className="max-w-md w-full space-y-8">
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Luo uusi käyttäjä
+            </h2>
+            <Form className="mt-8 space-y-6">
+              <div className="rounded-md shadow-sm -space-y-px">
                 <FormField
                   label="email"
                   name="email"
                   autoComplete="email"
                   required
+                  placeholder="Sähköposti"
+                  roundedTop
                 />
                 <FormField
                   label="password"
                   name="password"
                   required
                   type="password"
+                  placeholder="Salasana"
+                  roundedBottom
                 />
+              </div>
+              <div>
                 <button
                   type="submit"
                   disabled={isSubmitting}
-                  className="flex w-auto justify-center px-4 py-4 text-white rounded-md bg-indigo-500 "
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                  Register
+                  <span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
+                  Rekisteröi
                 </button>
               </div>
-            </div>
-          </Form>
-        )}
-      </Formik>
-    </div>
+            </Form>
+          </div>
+        </div>
+      )}
+    </Formik>
   );
 };
 
