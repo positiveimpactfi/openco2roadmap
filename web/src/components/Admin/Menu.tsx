342f58fa4328e835620c6c2f789f02206d372ecb
Yusif Salam-zade <yusalam@gmail.com>
2021-08-16 22:47:48 +0300

    frontend: change menu current tab color to green

--- web/src/components/Admin/Menu.tsx
+++ web/src/components/Admin/Menu.tsx
@@ -24,7 +24,7 @@ const Menu = () => {
         <select
           id="tabs"
           name="tabs"
-          className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm rounded-md"
+          className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm rounded-md"
           defaultValue={tabs.find((tab) => tab.current)?.name}
         >
           {adminLinks.map((tab) => (
@@ -44,7 +44,7 @@ const Menu = () => {
                 href={tab.href}
                 className={classNames(
                   tab.current
-                    ? "border-red-500 text-red-600"
+                    ? "border-green-500 text-green-600"
                     : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
                   "whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
                 )}
