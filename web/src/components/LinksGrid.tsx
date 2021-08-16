c20b1e64bcf725f1cc9d09568c9af779643108c0
Yusif Salam-zade <yusalam@gmail.com>
2021-08-16 14:47:48 +0300

    frontend: admin panel design tweaks

--- web/src/components/LinksGrid.tsx
+++ web/src/components/LinksGrid.tsx
@@ -2,26 +2,26 @@ import { adminLinks } from "data/adminLinks";
 
 function LinksGrid() {
   return (
-    <div className="bg-white">
-      <div className="max-w-4xl mx-auto px-4 py-16 sm:px-6 sm:pt-20 sm:pb-24 lg:max-w-7xl lg:pt-12 lg:px-8">
-        <div className="mt-2 grid grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-2 lg:mt-2 lg:grid-cols-4 lg:gap-x-8 lg:gap-y-16">
+    <div className="bg-gray-100">
+      <div className="max-w-4xl mx-auto px-4 py-16 sm:px-6 sm:pt-20 sm:pb-24 lg:max-w-7xl lg:pt-12 lg:px-8 bg-white">
+        <div className="mt-2 grid grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-2 lg:mt-2 ">
           {adminLinks.map((link) => (
             <div key={link.name}>
               <div className="flex flex-row">
-                <span className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500">
+                <span className="flex flex-shrink-0 items-center justify-center h-12 w-12 rounded-md bg-indigo-500">
                   <link.icon
                     className="h-6 w-6 text-white"
                     aria-hidden="true"
                   />
                 </span>
-                <h3 className="text-lg font-medium text-black ml-4">
-                  {link.name}
-                </h3>
-              </div>
-              <div className="mt-6">
-                <p className="mt-2 text-base text-gray-500">
-                  {link.description}
-                </p>
+                <div className="flex flex-col ml-4">
+                  <h3 className="text-lg font-medium text-black leading-none">
+                    {link.name}
+                  </h3>
+                  <p className="mt-2 text-base text-gray-500">
+                    {link.description}
+                  </p>
+                </div>
               </div>
             </div>
           ))}
