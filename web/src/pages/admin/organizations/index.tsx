73c80c3ab6548b24617e43b174c1720ec41abe75
Yusif Salam-zade <yusalam@gmail.com>
2021-08-17 10:02:40 +0300

    frontend: admin orgs table with fake data

--- web/src/pages/admin/organizations/index.tsx
+++ web/src/pages/admin/organizations/index.tsx
@@ -1,26 +1,139 @@
+import AdminsOnly from "components/Admin/AdminsOnly";
 import { useOrganizationsQuery } from "generated/graphql";
-import AdminPanel from "views/AdminPanel";
+import { DotsVerticalIcon } from "@heroicons/react/solid";
+import { number } from "yup";
 
 const Organizations = () => {
   const { data, loading } = useOrganizationsQuery();
   return (
-    <>
-      {loading ? (
-        <div> Loading...</div>
-      ) : (
-        <AdminPanel>
-          <div>
-            <ul>
-              {data?.organizations?.map((o) => (
-                <li key={o.id}>
-                  {o.name} {o.id}
-                </li>
-              ))}
-            </ul>
+    <AdminsOnly
+      title="Yritykset"
+      description="Tällä sivulla voit tarkastella ja muokata kaikkia laskurista löytyviä yrityksiä."
+    >
+      {loading ? <div> Ladataan...</div> : <OrganizationsTable />}
+    </AdminsOnly>
+  );
+};
+
+interface Organization {
+  id: number;
+  name: string;
+  businessId: string;
+  city: string;
+  destination: string;
+  created: string;
+  lastLoggedIn: string;
+}
+
+const OrganizationsTable = () => {
+  const org: Organization = {
+    id: 1,
+    name: "Matkailuyritys Oy",
+    businessId: "XXXXXX-X",
+    city: "Helsinki",
+    destination: "Uusimaa",
+    created: "10.10.2020",
+    lastLoggedIn: "17.08.2021",
+  };
+  const organizations: Organization[] = Array(20)
+    .fill(org)
+    .map((org, i) => {
+      return { ...org, id: org.id + i };
+    });
+
+  return (
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Yrityksen nimi
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Y-tunnus
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Kotikunta
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Destinaatio
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Luotu
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Kirjautunut
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Muokkaa
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {organizations.map((org) => (
+                  <tr key={org.id}>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      {org.name}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {org.businessId}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {org.city}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {org.destination}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {org.created}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {org.lastLoggedIn}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                      <button
+                        type="button"
+                        className="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      >
+                        <span className="sr-only">Open options</span>
+                        <DotsVerticalIcon
+                          className="w-5 h-5"
+                          aria-hidden="true"
+                        />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
-        </AdminPanel>
-      )}
-    </>
+        </div>
+      </div>
+    </div>
   );
 };
 
