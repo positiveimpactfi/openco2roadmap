0878f9a26a0ab74039142a5feb27cf6858e73c9f
Yusif Salam-zade <yusalam@gmail.com>
2021-08-17 11:40:42 +0300

    frontend: slideover design changes

--- web/src/components/SlideOver.tsx
+++ web/src/components/SlideOver.tsx
@@ -2,68 +2,71 @@ import { Dialog, Transition } from "@headlessui/react";
 import { XIcon } from "@heroicons/react/outline";
 import { Fragment } from "react";
 
-const SlideOver: React.FC<{ open: boolean; setOpen: (arg0: boolean) => void }> =
-  ({ open, setOpen, children }) => {
-    return (
-      <Transition.Root show={open} as={Fragment}>
-        <Dialog
-          as="div"
-          auto-reopen="true"
-          className="fixed inset-0 overflow-hidden"
-          onClose={setOpen}
-        >
-          <div className="absolute inset-0 overflow-hidden">
+const SlideOver: React.FC<{
+  title: string;
+  open: boolean;
+  setOpen: (arg0: boolean) => void;
+}> = ({ title, open, setOpen, children }) => {
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        auto-reopen="true"
+        className="fixed inset-0 overflow-hidden"
+        onClose={setOpen}
+      >
+        <div className="absolute inset-0 overflow-hidden">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-in-out duration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in-out duration-500"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-y-0 right-0 pl-10 max-w-full flex">
             <Transition.Child
               as={Fragment}
-              enter="ease-in-out duration-500"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="ease-in-out duration-500"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
+              enter="transform transition ease-in-out duration-500 sm:duration-700"
+              enterFrom="translate-x-full"
+              enterTo="translate-x-0"
+              leave="transform transition ease-in-out duration-500 sm:duration-700"
+              leaveFrom="translate-x-0"
+              leaveTo="translate-x-full"
             >
-              <Dialog.Overlay className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-            </Transition.Child>
-
-            <div className="fixed inset-y-0 right-0 pl-10 max-w-full flex">
-              <Transition.Child
-                as={Fragment}
-                enter="transform transition ease-in-out duration-500 sm:duration-700"
-                enterFrom="translate-x-full"
-                enterTo="translate-x-0"
-                leave="transform transition ease-in-out duration-500 sm:duration-700"
-                leaveFrom="translate-x-0"
-                leaveTo="translate-x-full"
-              >
-                <div className="w-screen max-w-md">
-                  <div className="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-scroll">
-                    <div className="px-4 sm:px-6">
-                      <div className="flex items-start justify-between">
-                        <Dialog.Title className="text-lg font-medium text-gray-900">
-                          Panel title
-                        </Dialog.Title>
-                        <div className="ml-3 h-7 flex items-center">
-                          <button
-                            className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                            onClick={() => setOpen(false)}
-                          >
-                            <span className="sr-only">Close panel</span>
-                            <XIcon className="h-6 w-6" aria-hidden="true" />
-                          </button>
-                        </div>
+              <div className="w-screen max-w-md">
+                <div className="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-scroll">
+                  <div className="px-4 sm:px-6 pb-4 border-b border-gray-100">
+                    <div className="flex items-start justify-between">
+                      <Dialog.Title className="text-lg font-medium text-gray-900">
+                        {title}
+                      </Dialog.Title>
+                      <div className="ml-3 h-7 flex items-center">
+                        <button
+                          className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                          onClick={() => setOpen(false)}
+                        >
+                          <span className="sr-only">Sulje</span>
+                          <XIcon className="h-6 w-6" aria-hidden="true" />
+                        </button>
                       </div>
                     </div>
-                    <div className="mt-6 relative flex-1 px-4 sm:px-6">
-                      {children}
-                    </div>
+                  </div>
+                  <div className="mt-6 relative flex-1 px-4 sm:px-6">
+                    {children}
                   </div>
                 </div>
-              </Transition.Child>
-            </div>
+              </div>
+            </Transition.Child>
           </div>
-        </Dialog>
-      </Transition.Root>
-    );
-  };
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+};
 
 export default SlideOver;
