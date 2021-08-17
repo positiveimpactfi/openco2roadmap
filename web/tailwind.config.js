062032e325da3675bcf1b834af87b9f505b6b0d7
Yusif Salam-zade <yusalam@gmail.com>
2021-08-17 11:29:00 +0300

    frontend: enable teal color

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   mode: "jit",
   purge: [
@@ -6,7 +8,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        teal: colors.teal,
+      },
+    },
   },
   variants: {
     extend: {},
