258d2e7a4ed4eb1eb91d236393d630b628fca710
Yusif Salam-zade <yusalam@gmail.com>
2021-08-20 11:02:08 +0300

    frontend: register page colors

--- web/src/components/Forms/Auth/RegisterForm.tsx
+++ web/src/components/Forms/Auth/RegisterForm.tsx
@@ -12,7 +12,7 @@ export const RegisterForm: React.FC<{ isSubmitting: boolean }> = ({
           name="email"
           autoComplete="email"
           required
-          placeholder="Sähköposti"
+          placeholder="Sähköpostiosoite"
           roundedTop
         />
         <FormField
@@ -28,7 +28,7 @@ export const RegisterForm: React.FC<{ isSubmitting: boolean }> = ({
         <button
           type="submit"
           disabled={isSubmitting}
-          className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
         >
           <span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
           Rekisteröi
