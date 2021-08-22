a1646d1156ebede3f400ff6761339b76b8bc0aa2
Yusif Salam-zade <yusalam@gmail.com>
2021-08-22 21:30:25 +0300

    frontend: fix isAdmin method

--- web/src/utils/isAdmin.ts
+++ web/src/utils/isAdmin.ts
@@ -4,5 +4,8 @@ export const isAdmin = (user: User): boolean => {
   if (!user) {
     return false;
   }
-  return user.roles[0].name === "ADMIN";
+
+  return user.roles.some(
+    (role) => role.name === "ADMIN" || role.name === "SUPERADMIN"
+  );
 };
