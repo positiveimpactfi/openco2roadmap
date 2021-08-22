7e252724a04b51cf4d67a80a42df8cc446ee01f1
Yusif Salam-zade <yusalam@gmail.com>
2021-08-22 23:48:14 +0300

    frontend: loading spinner

--- web/src/components/Admin/AdminsOnly.tsx
+++ web/src/components/Admin/AdminsOnly.tsx
@@ -1,11 +1,24 @@
-import { UserContext } from "context/UserContext";
-import { useContext } from "react";
+import { useMeQuery } from "generated/graphql";
+import Image from "next/image";
 import { isAdmin } from "utils/isAdmin";
 import AdminPanel from "views/AdminPanel";
+import logo from "../../../public/logo.svg";
 import { Headings } from "./Overview";
 
 const AdminsOnly: React.FC<Headings> = ({ title, description, children }) => {
-  const { user } = useContext(UserContext);
+  const { data, loading } = useMeQuery();
+
+  const user = data?.me;
+  if (loading) {
+    return (
+      <div className="flex flex-col w-96">
+        <div className="animate-spin mb-6">
+          <Image src={logo} alt="loading image" />
+        </div>
+        <div>Ladataan..</div>
+      </div>
+    );
+  }
 
   if (isAdmin(user)) {
     return (
