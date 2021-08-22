3400e19368c68775b1c6e2f8efa716ee49df7297
Yusif Salam-zade <yusalam@gmail.com>
2021-08-22 18:39:48 +0300

    backend: update initial admin user seed

--- server/src/migration/1622707251599-SeedAdmin.ts
+++ server/src/migration/1622707251599-SeedAdmin.ts
@@ -1,24 +1,39 @@
-import { Role, User } from "../entity/User";
+import { User } from "../entity/User";
+import { UserRole } from "../entity/UserRole";
 import argon2 from "argon2";
 import { MigrationInterface, QueryRunner } from "typeorm";
+import config from "../config";
+import { Organization } from "../entity/Organization";
 
 export class SeedAdmin1622707251599 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const role = await queryRunner.manager.save(Role, {
-      name: "ADMIN",
+    const adminOrg = await queryRunner.manager.save(Organization, {
+      name: "Admin org",
+      businessID: "XXXXXX-X",
     });
-    await queryRunner.manager.save(User, {
-      email: "admin@co2roadmap.com",
-      password: await argon2.hash("admin"),
+    const role = await queryRunner.manager.save(UserRole, {
+      name: "SUPERADMIN",
+      organizationId: adminOrg.id,
+    });
+    const adminUser = await queryRunner.manager.save(User, {
+      email: config.SUPERADMIN_EMAIL,
+      password: await argon2.hash(config.SUPERADMIN_PW),
       roles: [role],
-      firstName: "Admin",
-      lastName: "System",
+      firstName: "Super",
+      lastName: "Admin",
     });
+    adminOrg.users = [adminUser];
+    const savedAdminOrg = await queryRunner.manager.save(
+      Organization,
+      adminOrg
+    );
+
+    console.log("new admin organization created", savedAdminOrg);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.manager.delete(User, {
-      email: "admin@co2roadmap.com",
+      email: config.SUPERADMIN_EMAIL,
     });
   }
 }
