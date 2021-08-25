5993cd23b06dc4dc3bc232fa187e107bf0d080e6
Yusif Salam-zade <yusalam@gmail.com>
2021-08-25 16:09:56 +0300

    backend: EmissionFactor entity stub

---
+++ server/src/entity/EmissionFactor.ts
@@ -0,0 +1,36 @@
+import { Field, ID, ObjectType } from "type-graphql";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  // ManyToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+// import { EmissionSource } from "./EmissionSource";
+
+@ObjectType()
+@Entity()
+export class EmissionFactor extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Field()
+  @Column()
+  name: string;
+
+  @Field({ nullable: true })
+  @Column({ nullable: true })
+  source?: string;
+
+  @Field()
+  @Column()
+  geographicalArea: string;
+
+  // @Field(() => [EmissionSource])
+  // @ManyToMany(
+  //   () => EmissionSource,
+  //   (emissionSource) => emissionSource.emissionFactors
+  // )
+  // emissionSources: Promise<EmissionSource[]>;
+}
