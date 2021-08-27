906757b6c658ff730d877f59de3309b5bd72cb20
Yusif Salam-zade <yusalam@gmail.com>
2021-08-27 10:55:23 +0300

    backend: drop db function

---
+++ server/src/dropDb.ts
@@ -0,0 +1,12 @@
+import typeormConfig from "./ormconfig";
+import { createConnection } from "typeorm";
+
+const dropDb = async () => {
+  const conn = await createConnection({
+    ...typeormConfig,
+    host: "localhost",
+  });
+  await conn.dropDatabase();
+};
+
+dropDb();
