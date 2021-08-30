2a5d13328f6eebfc398405c303994464495c1cd4
Yusif Salam-zade <yusalam@gmail.com>
2021-08-30 14:19:05 +0300

    frontend: clean up Sidebar

--- web/src/components/Common/Sidebar.tsx
+++ web/src/components/Common/Sidebar.tsx
@@ -1,14 +1,11 @@
 import { Dialog, Transition } from "@headlessui/react";
 import {
-  CloudUploadIcon,
+  CalculatorIcon,
   CogIcon,
-  CollectionIcon,
   HomeIcon,
-  UserGroupIcon,
-  ViewGridIcon,
+  ServerIcon,
   XIcon,
 } from "@heroicons/react/outline";
-import { GlobeAltIcon, UsersIcon } from "@heroicons/react/solid";
 import NextImage from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -16,29 +13,11 @@ import { Dispatch, Fragment, SetStateAction } from "react";
 import { classNames } from "utils/classNames";
 import logoImg from "../../../public/logo.svg";
 
-const navigation = [
-  { name: "Home", href: "/admin", icon: HomeIcon },
-  {
-    name: "Organizations",
-    href: "/admin/organizations",
-    icon: GlobeAltIcon,
-  },
-  { name: "Users", href: "/admin/users", icon: UsersIcon },
-];
-
 const sidebarNavigation = [
   { name: "Etusivu", href: "/", icon: HomeIcon },
-  { name: "Dashboard", href: "#", icon: ViewGridIcon },
-  { name: "CO2-laskuri", href: "#", icon: CloudUploadIcon },
-  { name: "Käyttäjät", href: "#", icon: UserGroupIcon },
-  { name: "Data center", href: "#", icon: CollectionIcon },
+  { name: "Laskuri", href: "#", icon: CalculatorIcon },
   { name: "Asetukset", href: "#", icon: CogIcon },
-];
-
-const teams = [
-  { name: "Roadmap", href: "#", bgColorClass: "bg-indigo-500" },
-  { name: "Footprint", href: "#", bgColorClass: "bg-green-500" },
-  { name: "Handprint", href: "#", bgColorClass: "bg-yellow-500" },
+  { name: "Hallintapaneeli", href: "/admin", icon: ServerIcon },
 ];
 
 export interface AdminSidebarProps {
@@ -133,7 +112,7 @@ const MobileSideBar: React.FC<MobileSidebarProps> = ({
             <div className="mt-5 flex-1 h-0 overflow-y-auto">
               <nav className="px-2">
                 <div className="space-y-1">
-                  {navigation.map((item) => (
+                  {sidebarNavigation.map((item) => (
                     <Link href={item.href} passHref key={item.name}>
                       <a
                         key={item.name}
@@ -161,36 +140,6 @@ const MobileSideBar: React.FC<MobileSidebarProps> = ({
                       </a>
                     </Link>
                   ))}
-                </div>
-                <div className="mt-8">
-                  <h3
-                    className="px-3 text-xs font-semibold text-gray-500 uppercase tracking-wider"
-                    id="teams-headline"
-                  >
-                    Tags
-                  </h3>
-                  <div
-                    className="mt-1 space-y-1"
-                    role="group"
-                    aria-labelledby="teams-headline"
-                  >
-                    {teams.map((team) => (
-                      <a
-                        key={team.name}
-                        href={team.href}
-                        className="group flex items-center px-3 py-2 text-base leading-5 font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50"
-                      >
-                        <span
-                          className={classNames(
-                            team.bgColorClass,
-                            "w-2.5 h-2.5 mr-4 rounded-full"
-                          )}
-                          aria-hidden="true"
-                        />
-                        <span className="truncate">{team.name}</span>
-                      </a>
-                    ))}
-                  </div>
                 </div>
               </nav>
             </div>
