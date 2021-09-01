8f0685345c7ae29b6c3478a5e841b333802f12d2
Yusif Salam-zade <yusalam@gmail.com>
2021-09-01 12:16:08 +0300

    frontend: refactor Menu component

--- web/src/components/Admin/Menu.tsx
+++ web/src/components/Admin/Menu.tsx
@@ -1,63 +1,17 @@
-import { classNames } from "utils/classNames";
-import { adminLinks } from "data/adminLinks";
+import TabMenu from "components/TabMenu";
+import { adminLinks, PageLink } from "data/adminLinks";
 import { useRouter } from "next/router";
-import { PageLink } from "data/adminLinks";
 
 const Menu = () => {
   const router = useRouter();
-  const linksWithCurrent = adminLinks.map((link) => {
-    return { ...link, current: router.pathname.startsWith(link.href) };
-  });
   const currentTab: Partial<PageLink & { current: boolean }> = {
     name: "Hallintapaneeli",
     href: "/admin",
     current: router.pathname === "/admin",
   };
-  const tabs = [currentTab, ...linksWithCurrent];
+  const tabs = [currentTab, ...adminLinks];
 
-  return (
-    <div>
-      <div className="sm:hidden">
-        <label htmlFor="tabs" className="sr-only">
-          Select a tab
-        </label>
-        <select
-          id="tabs"
-          name="tabs"
-          className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm rounded-md"
-          defaultValue={tabs.find((tab) => tab.current)?.name}
-        >
-          {adminLinks.map((tab) => (
-            <option key={tab.name}>{tab.name}</option>
-          ))}
-        </select>
-      </div>
-      <div className="hidden sm:block">
-        <div className="border-b border-gray-200">
-          <nav
-            className="-mb-px flex space-x-8 overflow-x-auto"
-            aria-label="Tabs"
-          >
-            {tabs.map((tab) => (
-              <a
-                key={tab.name}
-                href={tab.href}
-                className={classNames(
-                  tab.current
-                    ? "border-green-500 text-green-600"
-                    : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
-                  "whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
-                )}
-                aria-current={tab.current ? "page" : undefined}
-              >
-                {tab.name}
-              </a>
-            ))}
-          </nav>
-        </div>
-      </div>
-    </div>
-  );
+  return <TabMenu links={tabs} />;
 };
 
 export default Menu;
---
+++ web/src/components/TabMenu.tsx
@@ -0,0 +1,62 @@
+import { PageLink } from "data/adminLinks";
+import { useRouter } from "next/router";
+import { classNames } from "utils/classNames";
+
+interface TabMenuProps {
+  links: Partial<PageLink & { current?: boolean }>[];
+}
+
+const TabMenu: React.FC<TabMenuProps> = ({ links }) => {
+  const router = useRouter();
+  const tabs = links.map((link) => {
+    return {
+      ...link,
+      current: link.current ?? router.pathname.startsWith(link.href),
+    };
+  });
+  return (
+    <div>
+      <div className="sm:hidden">
+        <label htmlFor="tabs" className="sr-only">
+          Select a tab
+        </label>
+        <select
+          id="tabs"
+          name="tabs"
+          className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm rounded-md"
+          defaultValue={tabs.find((tab) => tab.current)?.name}
+        >
+          {tabs.map((tab) => (
+            <option key={tab.name}>{tab.name}</option>
+          ))}
+        </select>
+      </div>
+      <div className="hidden sm:block">
+        <div className="border-b border-gray-200">
+          <nav
+            className="-mb-px flex space-x-8 overflow-x-auto"
+            aria-label="Tabs"
+          >
+            {tabs.map((tab) => (
+              <a
+                key={tab.name}
+                href={tab.href}
+                className={classNames(
+                  tab.current
+                    ? "border-green-500 text-green-600"
+                    : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
+                  "whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
+                )}
+                aria-current={tab.current ? "page" : undefined}
+              >
+                {tab.name}
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TabMenu;
