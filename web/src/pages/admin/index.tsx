e9bcfd3489ede818a0914a9a73c9641a72ef818d
Yusif Salam-zade <yusalam@gmail.com>
2021-09-03 16:53:03 +0300

    frontend: superadmins and admins can see disabled links

--- web/src/pages/admin/index.tsx
+++ web/src/pages/admin/index.tsx
@@ -1,8 +1,11 @@
 import AdminsOnly from "components/Admin/AdminsOnly";
 import LinksGrid from "components/LinksGrid";
 import { adminLinks } from "data/adminLinks";
+import { useUser } from "hooks/useUser";
+import { isAdmin } from "utils/isAdmin";
 
 const AdminHome = () => {
+  const { user } = useUser();
   const activeLinks = adminLinks.filter((link) => !link.disabled);
   return (
     <AdminsOnly
@@ -10,7 +13,7 @@ const AdminHome = () => {
       description="Näillä sivuilla voit muokata CO2-laskurin asetuksia. "
     >
       <div className="flex flex-col items-start">
-        <LinksGrid links={activeLinks} />
+        <LinksGrid links={isAdmin(user) ? adminLinks : activeLinks} />
       </div>
     </AdminsOnly>
   );
