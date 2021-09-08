14ff83948af18d31b37494b1e51bebaeb7c2bfdf
Yusif Salam-zade <yusalam@gmail.com>
2021-09-08 23:17:34 +0300

    frontend: start implementing create site form more settings

--- web/src/components/Forms/Site/CreateSiteForm.tsx
+++ web/src/components/Forms/Site/CreateSiteForm.tsx
@@ -1,11 +1,14 @@
+import { XIcon, CheckIcon } from "@heroicons/react/outline";
+import Button from "components/Button";
 import FormField from "components/Forms/Common/FormField";
 import Select from "components/Forms/Common/Select";
+import Table, { TableCell, TableCellOpenOptions } from "components/Table";
 import { municipalities } from "data/municipalities";
 import { Form, Formik, FormikProps } from "formik";
 import { useCreateSiteMutation } from "graphql/mutations/site/createSite.generated";
 import { MyOrganizationSitesDocument } from "graphql/queries/site/myOrganizationSites.generated";
 import { useMyOrganizationSiteTypesQuery } from "graphql/queries/site/myOrganizationSiteTypes.generated";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { Municipality, SiteType } from "types";
 
 interface FormValues {
@@ -23,6 +26,11 @@ const CreateSiteForm: React.FC<{
     municipality: null,
   };
   const [createSite] = useCreateSiteMutation();
+  const [showNewUnitButton, setShowNewUnitButton] = useState(false);
+  const [units, setUnits] = useState([]);
+  const [showInputField, setShowInputField] = useState(false);
+  const [newUnitValue, setNewUnitValue] = useState(null);
+
   const { data } = useMyOrganizationSiteTypesQuery();
   if (!data?.siteTypes) return <div>Cannot get site types</div>;
   return (
@@ -74,31 +82,110 @@ const CreateSiteForm: React.FC<{
               name="municipality"
               setFieldValue={setFieldValue}
             />
+            <button
+              type="button"
+              className="mt-4 text-sm"
+              onClick={() => setShowNewUnitButton(!showNewUnitButton)}
+            >
+              Lisäasetukset
+            </button>
+            {showNewUnitButton && (
+              <div className="flex flex-col space-y-4">
+                <div>
+                  <Button onClick={() => setShowInputField(!showInputField)}>
+                    Lisää uusi yksikkö
+                  </Button>
+                </div>
+                {showInputField && (
+                  <NewUnitInputField
+                    units={units}
+                    setUnits={setUnits}
+                    setOpen={setShowInputField}
+                  />
+                )}
+                <SiteUnitsTable units={units} />
+              </div>
+            )}
             <div className="pt-5">
-              <div className="flex justify-end">
-                <button
-                  type="button"
+              <div className="flex justify-end space-x-2">
+                <Button
                   onClick={() => {
                     handleReset();
                     setOpen(false);
                   }}
-                  className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
                 >
                   Peruuta
-                </button>
-                <button
-                  type="submit"
-                  disabled={isSubmitting}
-                  className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
-                >
+                </Button>
+                <Button variant="success" type="submit" disabled={isSubmitting}>
                   Tallenna
-                </button>
+                </Button>
               </div>
             </div>
           </div>
         </Form>
       )}
     </Formik>
+  );
+};
+
+const SiteUnitsTable: React.FC<{ units: string[] }> = ({ units }) => {
+  if (units.length === 0) return null;
+  return (
+    <Table headers={["Yksiköt", ""]}>
+      {units.map((unit, id) => (
+        <tr key={unit + id}>
+          <TableCell value={unit} />
+          <TableCellOpenOptions fn={() => console.log("opened edit")} />
+        </tr>
+      ))}
+    </Table>
+  );
+};
+
+const NewUnitInputField: React.FC<{
+  units: string[];
+  setUnits: (val: string[]) => void;
+  setOpen: (val: boolean) => void;
+}> = ({ units, setUnits, setOpen }) => {
+  const [unit, setUnit] = useState("");
+  return (
+    <div className="bg-gray-200 px-2 py-2 rounded-md">
+      <label
+        htmlFor="newUnitField"
+        className="block text-sm font-medium text-gray-700 mb-1"
+      >
+        Uuden yksikön nimi
+      </label>
+      <div className="flex">
+        <input
+          id="newUnitField"
+          placeholder="Uusi yksikkö"
+          className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-teal-500 focus:border-teal-500 focus:z-10 sm:text-sm rounded-md"
+          value={unit}
+          onChange={(e) => setUnit(e.currentTarget.value)}
+        />
+        <button
+          type="submit"
+          className="w-8 h-8 bg-white inline-flex items-center justify-center text-red-500 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+          onClick={() => {
+            setUnit(null);
+            setOpen(false);
+          }}
+        >
+          <XIcon className="w-5 h-5" aria-hidden="true" />
+        </button>
+        <button
+          type="button"
+          className="w-8 h-8 bg-teal-500 inline-flex items-center justify-center text-teal-500 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+          onClick={() => {
+            setUnits([...units, unit]);
+            setUnit("");
+          }}
+        >
+          <CheckIcon className="w-5 h-5" aria-hidden="true" />
+        </button>
+      </div>
+    </div>
   );
 };
 
