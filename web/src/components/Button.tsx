3fe413880d95ccaa2e6bf07ffc5c4335f19f1c44
Yusif Salam-zade <yusalam@gmail.com>
2021-09-08 15:50:40 +0300

    frontend: button color variants

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -1,10 +1,13 @@
 import { ButtonHTMLAttributes } from "react";
 import { classNames } from "utils/classNames";
 
+type ButtonVariant = "success" | "danger" | "neutral";
+
 interface ButtonProps {
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   disabled?: boolean;
   type?: ButtonHTMLAttributes<HTMLButtonElement>["type"];
+  variant?: ButtonVariant;
 }
 
 const Button: React.FC<ButtonProps> = ({
@@ -12,12 +15,18 @@ const Button: React.FC<ButtonProps> = ({
   disabled,
   type,
   children,
+  variant = "neutral",
 }) => {
   return (
     <button
       type={type ?? "button"}
       className={classNames(
-        "px-2 py-2 bg-teal-600 inline-flex items-center justify-center border border-transparent font-medium rounded-md text-white hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500",
+        "px-4 py-2  inline-flex items-center justify-center border  text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500",
+        variant === "neutral"
+          ? "bg-white border-gray-300 text-gray-700 hover:bg-gray-50"
+          : variant === "danger"
+          ? "bg-red-600 text-white border-transparent hover:bg-red-700"
+          : "bg-teal-600 text-white border-transparent hover:bg-teal-700 ",
         disabled ? "opacity-50 cursor-not-allowed" : null
       )}
       onClick={onClick}
