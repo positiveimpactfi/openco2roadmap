aa82573216cc31a84041b1f9ef0720dd6798a471
Yusif Salam-zade <yusalam@gmail.com>
2021-09-08 11:19:43 +0300

    frontend: update calculator link descriptions

--- web/src/data/calculatorLinks.ts
+++ web/src/data/calculatorLinks.ts
@@ -4,25 +4,29 @@ import { PageLink } from "./adminLinks";
 export const calculatorLinks: PageLink[] = [
   {
     name: "Toimipaikat",
-    description: "Lyhyt kuvaus",
+    description:
+      "Hiilijalanjälkilaskentaan tarvittavat kulutustiedot liittyvät aina konkreettisesti johonkin yrityksesi toimipaikkaan. Ennen kulutustietojen kirjaamista määrittele täällä yrityksesi toimipaikat ja niiden sijainnit.",
     icon: GlobeAltIcon,
     href: "/calculator/sites",
   },
   {
     name: "Päästolähteet",
-    description: "Lyhyt kuvaus",
+    description:
+      "Tutki mitä päästölähteitä yrityksesi laskentaan sisältyy. Voit muokata näihin liittyviä päästökertoimia asetuksissa.",
     icon: GlobeAltIcon,
     href: "/calculator/emission-sources",
   },
   {
     name: "Kulutustiedot",
-    description: "Lyhyt kuvaus",
+    description:
+      "Hiilijalanjäljen laskeminen perustuu yrityksesi todellisiin kulutustietoihin. Saat nämä mm. energiayhtiöiltä, ajopäiväkirjoista ja kirjanpidosta. sivun kautta syötät energian, jätteiden, hankintojen ja hallinnon eri kulutustiedot, kuukausi- tai vuositasolla.",
     icon: GlobeAltIcon,
     href: "/calculator/consumption-data",
   },
   {
     name: "Hiilijalanjäljet",
-    description: "Lyhyt kuvaus",
+    description:
+      "Päästölaskennan tuloksia pääset tutkimaan hiilijalanjäljet-sivulla.",
     icon: GlobeAltIcon,
     href: "/calculator/footprints",
   },
