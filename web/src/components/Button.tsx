1b7f71516f762cf5f54298b8609242359ef9c5f7
Yusif Salam-zade <yusalam@gmail.com>
2021-09-08 15:34:52 +0300

    frontend: button component more generic

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -1,12 +1,25 @@
+import { ButtonHTMLAttributes } from "react";
+import { classNames } from "utils/classNames";
+
 interface ButtonProps {
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  disabled?: boolean;
+  type?: ButtonHTMLAttributes<HTMLButtonElement>["type"];
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
+const Button: React.FC<ButtonProps> = ({
+  onClick,
+  disabled,
+  type,
+  children,
+}) => {
   return (
     <button
-      type="button"
-      className="px-2 py-2 mb-4 bg-teal-600 inline-flex items-center justify-center text-white rounded-md hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+      type={type ?? "button"}
+      className={classNames(
+        "px-2 py-2 bg-teal-600 inline-flex items-center justify-center border border-transparent font-medium rounded-md text-white hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500",
+        disabled ? "opacity-50 cursor-not-allowed" : null
+      )}
       onClick={onClick}
     >
       {children}
