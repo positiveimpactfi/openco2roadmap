01c37eecd7a2e0456e835407352c20cc69628467
Yusif Salam-zade <yusalam@gmail.com>
2021-09-09 10:23:57 +0300

    backend: seed categories with IDs

--- server/src/seed/data/categories.ts
+++ server/src/seed/data/categories.ts
@@ -1,12 +1,16 @@
-const cat = [
-  "Toimitilat ja kiinteistöt",
-  "Logistiikka",
-  "Hankinnat",
-  "Hallinto",
+import { CategoryType } from "../../types/CategoryType";
+import { StaticEntity } from "../../types/StaticEntity";
+
+const cat: StaticEntity[] = [
+  { name: "Toimitilat ja kiinteistöt", id: CategoryType.Toimitilat },
+  { name: "Logistiikka", id: CategoryType.Logistiikka },
+  { name: "Hankinnat", id: CategoryType.Hankinnat },
+  { name: "Hallinto", id: CategoryType.Hallinto },
 ];
 
 export const emissionCategories = cat.map((category) => {
   return {
-    name: category,
+    name: category.name,
+    id: category.id,
   };
 });
