e5d10d48e24079415f8d375e8c96f1f9373279bd
Yusif Salam-zade <yusalam@gmail.com>
2021-09-09 10:37:35 +0300

    backend: seed components with IDs

--- server/src/seed/data/components.ts
+++ server/src/seed/data/components.ts
@@ -1,62 +1,72 @@
 import { CategoryType } from "../../types/CategoryType";
+import { StaticEntity } from "../../types/StaticEntity";
+import { ComponentType } from "../../types/ComponentType";
 
-interface EmissionComponent {
-  name: string;
+type EmissionComponent = StaticEntity & {
   categoryID: number;
-}
+};
 
 export const emissionComponents: EmissionComponent[] = [
   {
     name: "Jäähdytys",
+    id: ComponentType.Jaahdytys,
     categoryID: CategoryType.Toimitilat,
   },
 
   {
     name: "Lämmitys",
+    id: ComponentType.Lammitys,
     categoryID: CategoryType.Toimitilat,
   },
   {
     name: "Sähkönkulutus",
+    id: ComponentType.Sahkonkulutus,
     categoryID: CategoryType.Toimitilat,
   },
   {
     name: "Jäte",
+    id: ComponentType.Jate,
     categoryID: CategoryType.Toimitilat,
   },
-  {
-    name: "Kiinteistöhuollon polttoaineet",
-    categoryID: CategoryType.Toimitilat,
-  },
-  {
-    name: "Tavarakuljetukset",
-    categoryID: CategoryType.Logistiikka,
-  },
+
   {
     name: "Asiakkaiden kuljetukset",
+    id: ComponentType.AsiakkaidenKuljetukset,
     categoryID: CategoryType.Logistiikka,
   },
   {
     name: "Työmatkat",
+    id: ComponentType.Tyomatkat,
+    categoryID: CategoryType.Logistiikka,
+  },
+  {
+    name: "Työssäkäyntimatkat",
+    id: ComponentType.Tyossakayntimatkat,
+    categoryID: CategoryType.Logistiikka,
+  },
+  {
+    name: "Tavarakuljetukset",
+    id: ComponentType.Tavarakuljetukset,
     categoryID: CategoryType.Logistiikka,
   },
   {
     name: "Elintarvikehankinnat",
-    categoryID: CategoryType.Hankinnat,
-  },
-  {
-    name: "Palveluhankinnat",
-    categoryID: CategoryType.Hankinnat,
-  },
-  {
-    name: "Muut hankinnat",
+    id: ComponentType.Elintarvikehankinnat,
     categoryID: CategoryType.Hankinnat,
   },
   {
     name: "Yleiset hankinnat",
+    id: ComponentType.YleisetHankinnat,
     categoryID: CategoryType.Hallinto,
   },
   {
-    name: "Työssäkäyntimatkat",
-    categoryID: CategoryType.Hallinto,
+    name: "Muut hankinnat",
+    id: ComponentType.MuutHankinnat,
+    categoryID: CategoryType.Hankinnat,
+  },
+  {
+    name: "Palveluhankinnat",
+    id: ComponentType.PalveluHankinnat,
+    categoryID: CategoryType.Hankinnat,
   },
 ];
---
+++ server/src/types/ComponentType.ts
@@ -0,0 +1,14 @@
+export enum ComponentType {
+  Jaahdytys = 1,
+  Lammitys = 2,
+  Sahkonkulutus = 3,
+  Jate = 4,
+  AsiakkaidenKuljetukset = 5,
+  Tyomatkat = 6,
+  Tyossakayntimatkat = 7,
+  Tavarakuljetukset = 8,
+  Elintarvikehankinnat = 9,
+  YleisetHankinnat = 10,
+  MuutHankinnat = 11,
+  PalveluHankinnat = 12,
+}
