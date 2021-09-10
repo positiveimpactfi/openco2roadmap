3cc9f52c25317fc5ddaf95289d8fd5c81b7f84d3
Yusif Salam-zade <yusalam@gmail.com>
2021-09-10 19:08:35 +0300

    backend: store creator org when creating a new emission factor value

--- server/src/resolvers/emissionFactorValue.ts
+++ server/src/resolvers/emissionFactorValue.ts
@@ -1,6 +1,8 @@
-import { Arg, Authorized, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Authorized, Ctx, Mutation, Query, Resolver } from "type-graphql";
 import { EmissionFactor } from "../entity/EmissionFactor";
 import { EmissionFactorValue } from "../entity/EmissionFactorValue";
+import { User } from "../entity/User";
+import { MyContext } from "../types/MyContext";
 import { Role } from "../types/Role";
 
 @Resolver(EmissionFactorValue)
@@ -14,11 +16,20 @@ export class EmissionFactorValueResolver {
   @Authorized([Role.SUPERADMIN, Role.ADMIN, Role.COMPANY_ADMIN])
   @Mutation(() => EmissionFactorValue)
   async createEmissionFactorValue(
+    @Ctx() { req }: MyContext,
     @Arg("emissionFactorID") emissionFactorID: string,
     @Arg("value") value: number,
     @Arg("startDate") startDate: number,
     @Arg("endDate") endDate: number
   ): Promise<EmissionFactorValue | undefined> {
+    const user = await User.findOne(req.session.userId, {
+      relations: ["organizations"],
+    });
+    if (!user) {
+      console.error("no user");
+      return undefined;
+    }
+    const org = user.organizations[0];
     const EF = await EmissionFactor.findOne(emissionFactorID);
     if (!EF) {
       console.error("no EF");
@@ -30,6 +41,7 @@ export class EmissionFactorValueResolver {
       startDate,
       endDate,
       emissionFactor: EF,
+      creator: org,
     }).save();
 
     (await EF.values).push(newEFValue);
