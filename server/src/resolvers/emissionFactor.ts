cceb4321a3c9876fc72d50b67fccb5af54600c58
Yusif Salam-zade <yusalam@gmail.com>
2021-09-10 19:09:56 +0300

    backend: query organizations own emission factors

--- server/src/resolvers/emissionFactor.ts
+++ server/src/resolvers/emissionFactor.ts
@@ -1,21 +1,73 @@
-import { Arg, Authorized, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Authorized, Ctx, Mutation, Query, Resolver } from "type-graphql";
 import { EmissionFactor } from "../entity/EmissionFactor";
+import { EmissionFactorValue } from "../entity/EmissionFactorValue";
 import { EmissionSource } from "../entity/EmissionSource";
 import { PhysicalQuantity } from "../entity/PhysicalQuantity";
+import { User } from "../entity/User";
 import { DataSourceType } from "../types/DataSourceType";
+import { MyContext } from "../types/MyContext";
 import { Role } from "../types/Role";
 
 @Resolver(EmissionFactor)
 export class EmissionFactorResolver {
   @Authorized([Role.SUPERADMIN, Role.ADMIN])
   @Query(() => [EmissionFactor])
-  allEmissionFactors() {
+  allEmissionFactors(): Promise<EmissionFactor[]> {
     return EmissionFactor.find({ relations: ["physicalQuantity"] });
+  }
+
+  @Query(() => [EmissionFactor])
+  async myOrganizationEmissionFactors(
+    @Ctx() { req }: MyContext
+  ): Promise<EmissionFactor[] | undefined> {
+    const user = await User.findOne(req.session.userId, {
+      relations: ["organizations"],
+    });
+    if (!user) {
+      console.error("no user");
+      return undefined;
+    }
+    const org = user.organizations[0];
+    const myEmissionFactorValues = await EmissionFactorValue.find({
+      where: { creator: org },
+      relations: [
+        "emissionFactor",
+        "emissionFactor.values",
+        "emissionFactor.values.creator",
+      ],
+    });
+    // groups organization's emission factor values by emission factor
+    const reduced = myEmissionFactorValues.reduce(async (previous, current) => {
+      const prev = await previous;
+      if (!prev) return;
+      if (prev.some((val) => val.name === current.emissionFactor.name)) {
+        const id = prev.findIndex(
+          (val) => val.name === current.emissionFactor.name
+        );
+        const copied = [...prev];
+        const vals = await copied[id].values;
+        const filteredValues = vals.filter((v) => v.creator?.id === org.id);
+        copied[id].values = Promise.resolve(filteredValues);
+        return copied;
+      } else {
+        const ef = current.emissionFactor;
+        const vals = (await ef.values).filter((v) => v.creator?.id === org.id);
+        const filteredEf = {
+          ...ef,
+          values: Promise.resolve(vals),
+        } as EmissionFactor;
+        console.log();
+        return [...prev, filteredEf];
+      }
+    }, Promise.resolve([]) as Promise<EmissionFactor[] | undefined>);
+
+    return reduced;
   }
 
   @Authorized([Role.SUPERADMIN, Role.ADMIN, Role.COMPANY_ADMIN])
   @Mutation(() => EmissionFactor)
   async createEmissionFactor(
+    @Ctx() { req }: MyContext,
     @Arg("emissionSourceIDs", () => [String]) emissionSourceIDs: string[],
     @Arg("name") name: string,
     @Arg("physicalQuantityID") physicalQuantityID: number,
@@ -23,6 +75,14 @@ export class EmissionFactorResolver {
     @Arg("dataSourceType", () => DataSourceType, { nullable: true })
     dataSourceType: DataSourceType
   ): Promise<EmissionFactor | undefined> {
+    const user = await User.findOne(req.session.userId, {
+      relations: ["organizations"],
+    });
+    if (!user) {
+      console.error("no user");
+      return undefined;
+    }
+    const org = user.organizations[0];
     const emissionSources = await EmissionSource.findByIds(emissionSourceIDs);
     if (!emissionSources) {
       console.error("no emission sources defined");
@@ -34,6 +94,7 @@ export class EmissionFactorResolver {
       physicalQuantity,
       source,
       dataSourceType,
+      creator: org,
     }).save();
     for (let emissionSource of emissionSources) {
       (await emissionSource.emissionFactors).push(newEF);
