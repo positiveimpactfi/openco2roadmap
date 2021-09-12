8f2c4e2a2f8fefad33e3bfaed3e8643966ed6c1d
Yusif Salam-zade <yusalam@gmail.com>
2021-09-12 18:59:24 +0300

    backend: all components resolver

--- server/src/resolvers/component.ts
+++ server/src/resolvers/component.ts
@@ -1,10 +1,12 @@
 import { Component } from "../entity/Component";
-import { Query, Resolver } from "type-graphql";
+import { Authorized, Query, Resolver } from "type-graphql";
+import { Role } from "../types/Role";
 
 @Resolver(Component)
 export class ComponentResolver {
+  @Authorized([Role.ADMIN, Role.COMPANY_ADMIN, Role.COMPANY_USER])
   @Query(() => [Component])
-  components(): Promise<Component[]> {
+  allComponents(): Promise<Component[]> {
     return Component.find({ relations: ["category"] });
   }
 }
