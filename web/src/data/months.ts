859cb3230573dd55d01e90659254a8bf43fe974d
Yusif Salam-zade <yusalam@gmail.com>
2021-09-13 11:49:00 +0300

    frontend: add more static client data

---
+++ web/src/data/months.ts
@@ -0,0 +1,50 @@
+export const months: { name: string; id: number }[] = [
+  {
+    name: "Tammikuu",
+    id: 1,
+  },
+  {
+    name: "Helmikuu",
+    id: 2,
+  },
+  {
+    name: "Maaliskuu",
+    id: 3,
+  },
+  {
+    name: "Huhtikuu",
+    id: 4,
+  },
+  {
+    name: "Toukokuu",
+    id: 5,
+  },
+  {
+    name: "Kesäkuu",
+    id: 6,
+  },
+  {
+    name: "Heinäkuu",
+    id: 7,
+  },
+  {
+    name: "Elokuu",
+    id: 8,
+  },
+  {
+    name: "Syyskuu",
+    id: 9,
+  },
+  {
+    name: "Lokakuu",
+    id: 10,
+  },
+  {
+    name: "Marraskuu",
+    id: 11,
+  },
+  {
+    name: "Joulukuu",
+    id: 12,
+  },
+];
