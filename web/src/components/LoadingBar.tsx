c88ff120ef11ba325b9e08a09d96273d5106474d
Yusif Salam-zade <yusalam@gmail.com>
2021-09-16 12:18:37 +0300

    frontend: thinner loading bar

--- web/src/components/LoadingBar.tsx
+++ web/src/components/LoadingBar.tsx
@@ -1,7 +1,7 @@
 const LoadingBar = () => {
   return (
     <div
-      className="h-2 w-screen ml-[112px]
+      className="h-1 w-screen ml-[112px]
      fixed top-0 left-0 after:absolute overflow-hidden bg-teal-500 after:inset-0 -after:translate-x-full after:bg-gradient-to-r after:from-teal-300 after:to-teal-500 after:animate-shimmer z-50000
   "
     />
