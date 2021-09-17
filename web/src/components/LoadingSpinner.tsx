0b95d50cfe7e625257b704b3479a235798c543dd
Yusif Salam-zade <yusalam@gmail.com>
2021-09-17 21:42:11 +0300

    frontend: only show loading spinner if loading takes more than 2s

--- web/src/components/LoadingSpinner.tsx
+++ web/src/components/LoadingSpinner.tsx
@@ -1,7 +1,16 @@
-import logo from "../../public/logo.svg";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import logo from "../../public/logo.svg";
 
-const LoadingSpinner = () => {
+const LoadingSpinner: React.FC<{ delay?: number }> = ({ delay = 2000 }) => {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    const timerId = setTimeout(() => setShow(true), delay);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [setShow, delay]);
+  if (!show) return null;
   return (
     <div className="flex flex-col">
       <div className="animate-spin mb-6">
