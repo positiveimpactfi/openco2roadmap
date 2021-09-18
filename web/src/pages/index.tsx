4838e8fb3a28549659084da52c73f52439610113
Yusif Salam-zade <yusalam@gmail.com>
2021-09-18 14:07:10 +0300

    frontend: improve home page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,6 +3,8 @@ import { useMeQuery } from "graphql/queries/users/me.generated";
 import { User } from "types/generatedTypes";
 import Link from "next/link";
 import { withAuth } from "components/Auth";
+import LinksGrid from "components/LinksGrid";
+import { sidebarLinks } from "data/links/sidebarLinks";
 
 const Home = () => {
   const { data, loading } = useMeQuery();
@@ -16,13 +18,12 @@ const Home = () => {
 
 const UserLoggedIn: React.FC<{ user: Partial<User> }> = ({ user }) => {
   return (
-    <div className="flex flex-col h-full items-center justify-center w-full flex-1 px-20 text-center">
-      <div>{`Moi, ${user.email}`}</div>
-      <Link href="/admin" passHref>
-        <a className="font-medium text-teal-600 hover:text-teal-500">
-          Siirry admin dashbordille
-        </a>
-      </Link>
+    <div className="px-4 sm:px-6 lg:px-8 pb-4">
+      <h2 className="text-xl mb-4">{`Moi, ${user.firstName ?? user.email}`}</h2>
+
+      <div className="flex flex-col items-start">
+        <LinksGrid links={sidebarLinks} />
+      </div>
     </div>
   );
 };
