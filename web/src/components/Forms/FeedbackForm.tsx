9ba5e4085a8387b80db2b7560fbfbf5aca41269d
Yusif Salam-zade <yusalam@gmail.com>
2021-09-19 19:07:50 +0300

    frontend: working give feedback

--- web/src/components/Forms/FeedbackForm.tsx
+++ web/src/components/Forms/FeedbackForm.tsx
@@ -1,42 +1,72 @@
 import Button from "components/Button";
-import { Formik, Form, Field } from "formik";
+import { Field, Form, Formik } from "formik";
+import { useUser } from "hooks/useUser";
+import { useRouter } from "next/router";
 
-const FeedbackForm = () => {
+const FeedbackForm: React.FC<{
+  setShowNotification: (arg: boolean) => void;
+  setShowModal: (arg: boolean) => void;
+}> = ({ setShowNotification, setShowModal }) => {
+  const { user } = useUser();
+  const router = useRouter();
+
   return (
-    <Formik
-      initialValues={{ feedback: "" }}
-      onSubmit={() => console.log("form submitted")}
-    >
-      {() => (
-        <Form className="mt-8">
-          <div className="rounded-md">
-            <div className="mt-1 sm:mt-0 sm:col-span-2">
-              <p className="mt-2 text-sm text-gray-500">
-                Kirjoita tähän kenttään palautteesi:
-              </p>
-              <Field
-                id="feedback"
-                name="feedback"
-                rows={10}
-                className="max-w-lg shadow-sm block w-full focus:ring-teal-500 focus:border-teal-500 sm:text-sm border border-gray-300 rounded-md"
-                as="textarea"
-              />
+    <>
+      <Formik
+        initialValues={{ feedback: "" }}
+        onSubmit={async ({ feedback }) => {
+          const requestOptions = {
+            method: "POST",
+            body: JSON.stringify({
+              text: `
+            ========================================
+            New feedback received!\n
+            User: ${user.email}\n
+            URL: ${router.pathname}\n
+            Date: ${new Date().toLocaleString("fi")}\n
+            Feedback: ${feedback}\n
+            ========================================`,
+            }),
+          };
+          const response = await fetch(
+            process.env.NEXT_PUBLIC_SLACK_WEBHOOK,
+            requestOptions
+          );
+          if (response.ok) {
+            setShowModal(false);
+            setShowNotification(true);
+            console.log("form submitted, response", response);
+          }
+        }}
+      >
+        {() => (
+          <Form className="mt-8">
+            <div className="rounded-md">
+              <div className="mt-1 sm:mt-0 sm:col-span-2">
+                <p className="mt-2 text-sm text-gray-500">
+                  Kirjoita tähän kenttään palautteesi:
+                </p>
+                <Field
+                  id="feedback"
+                  name="feedback"
+                  rows={10}
+                  className="max-w-lg shadow-sm block w-full focus:ring-teal-500 focus:border-teal-500 sm:text-sm border border-gray-300 rounded-md"
+                  as="textarea"
+                />
+              </div>
+              <div className="mt-5 sm:flex space-x-2">
+                <Button variant="success" type="submit">
+                  Lähetä
+                </Button>
+                <Button onClick={() => console.log("form closed")}>
+                  Peruuta
+                </Button>
+              </div>
             </div>
-            <div className="mt-5 sm:flex space-x-2">
-              <Button
-                variant="success"
-                onClick={() => console.log("button clicked")}
-              >
-                Lähetä
-              </Button>
-              <Button onClick={() => console.log("form closed")}>
-                Peruuta
-              </Button>
-            </div>
-          </div>
-        </Form>
-      )}
-    </Formik>
+          </Form>
+        )}
+      </Formik>
+    </>
   );
 };
 
--- web/src/components/Layout/GiveFeedback.tsx
+++ web/src/components/Layout/GiveFeedback.tsx
@@ -1,18 +1,29 @@
 import { ReplyIcon } from "@heroicons/react/outline";
 import FeedbackForm from "components/Forms/FeedbackForm";
 import Modal from "components/Modal";
+import Notification from "components/Notification";
 import { useState } from "react";
 
 const GiveFeedback = () => {
-  const [open, setOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [showNotification, setShowNotification] = useState(false);
   return (
     <>
-      <Modal open={open} setOpen={setOpen} title="Anna palautetta">
-        <FeedbackForm />
+      <Modal open={modalOpen} setOpen={setModalOpen} title="Anna palautetta">
+        <FeedbackForm
+          setShowNotification={setShowNotification}
+          setShowModal={setModalOpen}
+        />
       </Modal>
+      <Notification
+        title="Kiitos palautteestasi"
+        description="Olemme vastaanottaneet palautteesi"
+        show={showNotification}
+        setShow={setShowNotification}
+      />
       <div className="fixed right-8 bottom-8 w-10 h-10 border border-gray-600 px-2 py-2 hover:text-white hover:bg-teal-500 rounded-md">
         <span className="sr-only">Avaa anna palautetta lomake</span>
-        <button onClick={() => setOpen(true)} className="w-full h-full">
+        <button onClick={() => setModalOpen(true)} className="w-full h-full">
           <ReplyIcon />
         </button>
       </div>
