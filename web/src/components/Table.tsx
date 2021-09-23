1731bbabc3c23b6a321396190dc3ae4eace0c7c9
Yusif Salam-zade <yusalam@gmail.com>
2021-09-23 17:01:24 +0300

    frontend: editable table cell

--- web/src/components/Table.tsx
+++ web/src/components/Table.tsx
@@ -1,9 +1,12 @@
+import { CheckIcon, PencilAltIcon, TrashIcon } from "@heroicons/react/outline";
 import { DotsVerticalIcon } from "@heroicons/react/solid";
+import { useState } from "react";
 import { classNames } from "utils/classNames";
 interface TableProps {
   headers: string[];
   alignLastRight?: boolean;
 }
+import { XIcon } from "@heroicons/react/outline";
 
 const Table: React.FC<TableProps> = ({
   headers,
@@ -51,7 +54,74 @@ export const TableCell: React.FC<{
   );
 };
 
-export const TableCellOpenOptions: React.FC<{ fn: any }> = ({ fn }) => {
+export const TableCellWithEdit: React.FC<{
+  value: string;
+  clamped?: boolean;
+  hideOnSm?: boolean;
+}> = ({ value, clamped = false, hideOnSm = false }) => {
+  const [newValue, setNewValue] = useState(value);
+  const [changedValue, setChangedValue] = useState("");
+  const [edit, setEdit] = useState(false);
+  return (
+    <div>
+      <td
+        className={classNames(
+          "px-6 py-4  text-sm font-medium text-gray-500",
+          clamped ? `max-w-[40px] md:max-w-[70px] truncate` : null,
+          hideOnSm ? "hidden md:visible" : null
+        )}
+      >
+        {edit ? (
+          <span className="flex">
+            <input
+              placeholder="Uusi nimi"
+              onChange={(e) => setChangedValue(e.target.value)}
+              className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-teal-500 focus:border-teal-500 focus:z-10 sm:text-sm rounded-md"
+            />{" "}
+            <div className="absolute right-20">
+              <button
+                type="button"
+                className="w-8 h-8 bg-white inline-flex items-center justify-center text-red-500 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+                onClick={() => {
+                  setEdit(false);
+                }}
+              >
+                <XIcon className="w-5 h-5 text-red-500" aria-hidden="true" />
+              </button>
+              <button
+                type="button"
+                onClick={() => {
+                  setNewValue(changedValue);
+                  setEdit(false);
+                }}
+                className="w-8 h-8 bg-teal-500 inline-flex items-center justify-center text-teal-500 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+              >
+                <CheckIcon className="w-5 h-5 text-teal-500" />
+              </button>
+            </div>
+          </span>
+        ) : (
+          newValue
+        )}
+        {!edit && (
+          <button
+            type="button"
+            className="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 absolute right-20"
+            onClick={() => setEdit(true)}
+          >
+            <span className="sr-only">Muokkaa yksikön nimeä</span>
+            <PencilAltIcon className="w-5 h-5" aria-hidden="true" />
+          </button>
+        )}
+      </td>
+    </div>
+  );
+};
+
+export const TableCellOpenOptions: React.FC<{
+  fn: any;
+  variant?: "edit" | "delete" | "expand";
+}> = ({ fn, variant = "expand" }) => {
   return (
     <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
       <button
@@ -60,7 +130,13 @@ export const TableCellOpenOptions: React.FC<{ fn: any }> = ({ fn }) => {
         onClick={fn}
       >
         <span className="sr-only">Avaa sivupalkki</span>
-        <DotsVerticalIcon className="w-5 h-5" aria-hidden="true" />
+        {variant === "expand" ? (
+          <DotsVerticalIcon className="w-5 h-5" aria-hidden="true" />
+        ) : variant === "edit" ? (
+          <PencilAltIcon className="w-5 h-5" aria-hidden="true" />
+        ) : (
+          <TrashIcon className="w-5 h-5" aria-hidden="true" />
+        )}
       </button>
     </td>
   );
