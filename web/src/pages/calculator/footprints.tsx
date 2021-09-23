09774cbc18d7a55d7b22627fe771e2b576f8bb1d
Yusif Salam-zade <yusalam@gmail.com>
2021-09-23 03:30:59 +0300

    frontend: calculator footprints table

--- web/src/pages/calculator/footprints.tsx
+++ web/src/pages/calculator/footprints.tsx
@@ -1,8 +1,33 @@
+import { emissionCategories } from "@/shared/categories";
 import { withAuth } from "components/Auth";
 import CalculatorPanel from "components/CalculatorPanel";
+import Table, { TableCell } from "components/Table";
 import { PageUnderConstruction } from "components/UnderConstruction";
+import { useMyOrganizationEmissionsByCategoryAndYearQuery } from "graphql/queries/emissions/myOrganizationEmissionsByCategoryAndYear.generated";
+import { numberToString } from "utils/numberToString";
 
 const CalculatorFootprintsPage = () => {
+  const { data } = useMyOrganizationEmissionsByCategoryAndYearQuery();
+  const components = data?.myOrganizationEmissionsByCategoryAndYear;
+  const allYears = components?.map((c) => JSON.parse(c.yearlysums)) as {
+    [key: number]: number;
+  }[];
+  const allYearsParsed = allYears
+    ?.reduce<number[]>((previous, current) => {
+      const ke = Object.keys(current)
+        .map((k) => parseInt(k))
+        .flat();
+      const prevCopy = [...previous];
+      for (let k of ke) {
+        if (!previous.includes(k)) {
+          prevCopy.push(k);
+        }
+      }
+
+      return prevCopy;
+    }, [])
+    .sort();
+  const allParsedString = allYearsParsed?.map((y) => y.toString());
   return (
     <CalculatorPanel
       title="Hiilijalanjäljet"
@@ -11,6 +36,28 @@ const CalculatorFootprintsPage = () => {
       <div className="flex justify-center items-center h-full">
         <PageUnderConstruction />
       </div>
+      <Table headers={["Hiilijalanjäljen Yhteenveto"].concat(allParsedString)}>
+        {components?.map((c, i) => (
+          <tr key={c.categoryid}>
+            <TableCell
+              key={c.categoryid.toString() + i.toString()}
+              value={
+                emissionCategories.find(
+                  (category) => category.id === parseInt(c.categoryid)
+                ).name
+              }
+            />
+            {/* <TableCell value={c.yearlysums} /> */}
+            {allYearsParsed?.map((y) => (
+              <TableCell
+                key={c.categoryid.toString() + y.toString()}
+                value={allYears[i][y]?.toFixed(1) ?? ""}
+                clamped
+              />
+            ))}
+          </tr>
+        ))}
+      </Table>
     </CalculatorPanel>
   );
 };
