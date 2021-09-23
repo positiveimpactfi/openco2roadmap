d362716d141bfe2a473baed0a0789440ae52ea8f
Yusif Salam-zade <yusalam@gmail.com>
2021-09-23 15:06:10 +0300

    backend: simple update site mutation that does not update site units

--- server/src/resolvers/site.ts
+++ server/src/resolvers/site.ts
@@ -7,6 +7,7 @@ import {
   Query,
   Resolver,
 } from "type-graphql";
+import { z } from "zod";
 import { Municipality } from "../entity/Municipality";
 import { Site } from "../entity/Site";
 import { SiteType } from "../entity/SiteType";
@@ -14,6 +15,15 @@ import { SiteUnit } from "../entity/SiteUnit";
 import { User } from "../entity/User";
 import { Role } from "../types";
 import { MyContext } from "../types/MyContext";
+import { truthyObject } from "../utils/truthyObject";
+
+const updateSchema = z
+  .object({
+    name: z.string(),
+    siteTypeID: z.string(),
+    municipalityID: z.number(),
+  })
+  .partial();
 
 @Resolver(Site)
 export class SiteResolver {
@@ -58,6 +68,46 @@ export class SiteResolver {
     }
     await newSite.save();
     return newSite;
+  }
+
+  @Authorized([Role.ADMIN, Role.COMPANY_ADMIN])
+  @Mutation(() => Site)
+  async updateSite(
+    @Arg("siteID") siteID: string,
+    @Arg("name", { nullable: true }) name: string,
+    @Arg("siteTypeID", { nullable: true }) siteTypeID: string,
+    @Arg("municipalityID", () => Int, { nullable: true }) municipalityID: number
+    // @Arg("siteUnits", () => [String], { nullable: true }) siteUnits?: string[]
+  ): Promise<Site | undefined> {
+    const site = await Site.findOne(siteID, {
+      relations: ["siteUnits", "siteType"],
+    });
+    if (!site) {
+      console.error("invalid site");
+      return undefined;
+    }
+    const res = updateSchema.safeParse({ name, siteTypeID, municipalityID });
+    if (!res.success) {
+      console.error("bad update site input data");
+      return undefined;
+    }
+    const {
+      name: pName,
+      siteTypeID: pSiteTypeID,
+      municipalityID: pMunicipalityID,
+    } = res.data;
+    const siteData = {
+      ...(pName && { name: pName }),
+      ...(pSiteTypeID && { siteType: await SiteType.findOne(pSiteTypeID) }),
+      ...(pMunicipalityID && {
+        municipality: await Municipality.findOne(pMunicipalityID),
+      }),
+    };
+    /** siteData fields can be undefined (if findOne fails), so we pass only truthy fields to the update function  */
+    await Site.update(site.id, truthyObject(siteData));
+    await site.reload();
+
+    return site;
   }
 
   @Authorized([Role.ADMIN])
