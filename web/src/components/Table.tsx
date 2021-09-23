27bdbf8b5788a44551bd6d66f04c7d5f90725e02
Yusif Salam-zade <yusalam@gmail.com>
2021-09-23 03:29:19 +0300

    frontend: table has unique keys in table head

--- web/src/components/Table.tsx
+++ web/src/components/Table.tsx
@@ -14,9 +14,9 @@ const Table: React.FC<TableProps> = ({
     <table className="min-w-full divide-y divide-gray-200">
       <thead className="bg-gray-50">
         <tr>
-          {headers.map((header) => (
+          {headers.map((header, i) => (
             <th
-              key={header}
+              key={header + i}
               scope="col"
               className={classNames(
                 "px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider",
