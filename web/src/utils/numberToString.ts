f59c55d5099d1c9863c4bca28e5c38f4db23cfcb
Yusif Salam-zade <yusalam@gmail.com>
2021-09-30 14:10:38 +0300

    frontend: fix localized number format not working with very large and very small numbers

--- web/src/utils/numberToString.ts
+++ web/src/utils/numberToString.ts
@@ -3,20 +3,28 @@ export const numberToString = (x: number): string => {
     if (x === 0) return "0";
 
     const numToExp = x.toExponential(2);
-    const exp = parseInt(numToExp.toLocaleString().split("e-")[1]);
+    const exp = parseInt(numToExp.toString().split("e-")[1]);
     if (exp > 3 && exp <= 5) return numToExp;
 
-    let e = parseInt(x.toLocaleString().split("e-")[1]);
+    let e = parseInt(x.toString().split("e-")[1]);
     if (e) {
       return (
-        parseFloat(x.toLocaleString().split("e-")[0]).toFixed(2) + "e-" + e
+        parseFloat(
+          parseFloat(x.toString().split("e-")[0]).toFixed(2)
+        ).toLocaleString() +
+        "e-" +
+        e
       );
     }
   } else {
-    let e = parseInt(x.toLocaleString().split("e+")[1]);
+    let e = parseInt(x.toString().split("e+")[1]);
     if (e) {
       return (
-        parseFloat(x.toLocaleString().split("e+")[0]).toFixed(2) + "e-" + e
+        parseFloat(
+          parseFloat(x.toString().split("e+")[0]).toFixed(2)
+        ).toLocaleString() +
+        "e-" +
+        e
       );
     }
   }
