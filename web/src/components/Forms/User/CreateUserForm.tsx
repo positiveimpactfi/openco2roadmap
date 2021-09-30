bda309db1f0b6b616da167bf7db4e19f56faba54
Yusif Salam-zade <yusalam@gmail.com>
2021-09-30 15:35:09 +0300

    frontend: close new user form when cancel button is clicked

--- web/src/components/Forms/User/CreateUserForm.tsx
+++ web/src/components/Forms/User/CreateUserForm.tsx
@@ -96,7 +96,7 @@ const CreateUserForm: React.FC<{ setOpen: (val: boolean) => void }> = ({
                   type="button"
                   onClick={() => {
                     handleReset();
-                    // setSlideoverOpen(false);
+                    setOpen(false);
                   }}
                   className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
                 >
