ea2b7c2d63357c96968425c05c2892be60bd8e9d
Yusif Salam-zade <yusalam@gmail.com>
2021-10-07 15:50:44 +0300

    frontend: show all emission factors on admin panel emission factors page

--- web/src/pages/admin/emission-factors.tsx
+++ web/src/pages/admin/emission-factors.tsx
@@ -1,10 +1,73 @@
 import AdminsOnly from "components/Admin/AdminsOnly";
 import { withAuth } from "components/Auth";
+import Button from "components/Button";
+import LoadingSpinner from "components/LoadingSpinner";
+import Table, { TableCell, TableCellOpenOptions } from "components/Table";
+import { useAllEmissionFactorsQuery } from "graphql/queries/emissions/allEmissionFactors.generated";
+import { numberToString } from "utils/numberToString";
 
 const AdminEmissionFactorsPage = () => {
+  const { data, loading } = useAllEmissionFactorsQuery();
+  const emissionFactors = data?.allEmissionFactors;
   return (
-    <AdminsOnly title="Päästökertoimet" description="">
-      <div>ef</div>
+    <AdminsOnly
+      title="Päästökertoimet"
+      description="Tällä sivulla voit tarkastella kaikki järjestelmän päästökertoimet ja määritellä uusia."
+    >
+      <div className="mb-4">
+        <Button
+          variant="success"
+          disabled
+          onClick={() => console.log("clicked create new ef button")}
+        >
+          Lisää uusi kerroin
+        </Button>
+      </div>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <Table
+          headers={[
+            "Nimi",
+            "Lähde",
+            "Alkaen",
+            "Päättyen",
+            "Uusin arvo",
+            "Luonut",
+            "Tiedot",
+          ]}
+          alignLastRight
+        >
+          {emissionFactors?.map((ef) => (
+            <tr key={ef.id}>
+              <TableCell value={ef.name} />
+              <TableCell value={ef.source} />
+              <TableCell
+                value={[...ef.values]
+                  .sort((a, b) => a.startDate - b.startDate)[0]
+                  .startDate.toString()}
+              />
+              <TableCell
+                value={[...ef.values]
+                  .sort((a, b) => b.endDate - a.endDate)[0]
+                  .endDate.toString()}
+              />
+              <TableCell
+                value={
+                  numberToString(
+                    [...ef.values].sort((a, b) => b.endDate - a.endDate)[0]
+                      .value
+                  ) +
+                  " kg CO2e/" +
+                  ef.physicalQuantity.baseUnit.shorthand
+                }
+              />
+              <TableCell value={ef.creator ? ef.creator.name : "-"} />
+              <TableCellOpenOptions fn={() => console.log("opened ef")} />
+            </tr>
+          ))}
+        </Table>
+      )}
     </AdminsOnly>
   );
 };
