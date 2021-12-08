8427e0ac8ff95fe3fe14a1b10c1bc3a83184900c
Yusif Salam-zade <yusalam@gmail.com>
2021-12-08 15:30:32 +0200

    feat (api): user invitation email template

---
+++ server/src/utils/templates/email/userInvitation.ts
@@ -0,0 +1,34 @@
+import config from "../../../config";
+import { User } from "../../../entity";
+
+export const userInvitationEmail = (user: User, token: string): string => {
+  return `
+    <div>
+    <div>Hei,</div>
+    <br/>
+    <div>${
+      user.organizations[0].name
+    } on ottanut matkailualan avoimen hiilijalanjälkilaskurin käyttöön, ja ${
+    user.firstName && user.lastName
+      ? user.firstName + " " + user.lastName
+      : user.email
+  } on nyt kutsunut sinut osallistumaan työhön.</div>
+    <br/>
+    <div>Voit rekisteröityä laskurin käyttäjäksi seuraavan linkin kautta:</div>
+    <br/>
+    <div><span><a href="${config.CORS_ORIGIN}/register/${token}">${
+    config.CORS_ORIGIN
+  }/register/${token}</a></span></div>
+    <br/>
+    <div>Jos sinulla on kysyttävää asiasta, voit olla yhteydessä kutsun lähettäjään sähköpostiosoitteella ${
+      user.email
+    }.</div>
+    <div>Saat lisätietoja laskurista osoitteesta <span><a href="https://app.co2roadmap.fi">XXXXXXX.fi</a></span><div>
+    <br />
+    <div>Tervetuloa!<div>
+    <br />
+    <div>Ystävällisin terveisin</div>
+    <div>OpenCO2Roadmap tiimi</div>
+    </div>
+    `;
+};
