87b9ff424f96e11e7781e89f31eb7ad0063c28fa
Yusif Salam-zade <yusalam@gmail.com>
2021-12-20 10:54:58 +0200

    feat (api): enable logging with pino

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,14 +10,41 @@ import { createConnection } from "typeorm";
 import config from "./config";
 import typeormConfig from "./ormconfig";
 import { resolvers } from "./resolvers";
-// import { requestLogger } from "./utils/requestLogger";
 import { authChecker } from "./utils/authChecker";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import pinoHttp from "pino-http";
+import childProcess from "child_process";
+import stream from "stream";
+
+const cwd = process.cwd();
+const { env } = process;
+const logThrough = new stream.PassThrough();
+const logsPath = `${cwd}/logs`;
+
+const child = childProcess.spawn(
+  process.execPath,
+  [
+    require.resolve("pino-tee"),
+    "warn",
+    `${logsPath}/warn.log`,
+    "error",
+    `${logsPath}/error.log`,
+    "fatal",
+    `${logsPath}/fatal.log`,
+    "debug",
+    `${logsPath}/debug.log`,
+  ],
+  { cwd, env }
+);
+
+logThrough.pipe(child.stdin);
 
 const main = async () => {
   await createConnection(typeormConfig);
   // await conn.runMigrations();
   const app = express();
+  const pino = pinoHttp({ name: "openco2roadmap" }, logThrough);
+  app.use(pino);
   app.use(express.json());
 
   const RedisStore = connectRedis(session);
@@ -34,8 +61,6 @@ const main = async () => {
       credentials: true,
     })
   );
-
-  // app.use(requestLogger);
 
   app.use(
     session({
