d8716e57b183c040872d103ce0ed9acafdda4199
Yusif Salam-zade <yusalam@gmail.com>
2022-01-19 15:12:34 +0200

    wip (server): RegistrationRequest resolver

---
+++ server/src/resolvers/registrationRequest.ts
@@ -0,0 +1,75 @@
+import {
+  Arg,
+  Authorized,
+  Field,
+  InputType,
+  Int,
+  Mutation,
+  Query,
+  Resolver,
+} from "type-graphql";
+import { BusinessField, Municipality } from "../entity";
+import { RegistrationRequest } from "../entity/RegistrationRequest";
+
+@InputType()
+class RegistrationRequestInput implements Partial<RegistrationRequest> {
+  @Field()
+  lastName: string;
+
+  @Field()
+  firstName: string;
+
+  @Field()
+  email: string;
+
+  @Field()
+  businessID: string;
+
+  @Field(() => Int, { nullable: true })
+  businessFieldID: number;
+
+  @Field(() => Int, { nullable: true })
+  municipalityID: number;
+}
+
+@Resolver(RegistrationRequest)
+export class RegistrationRequestResolver {
+  @Authorized([])
+  @Query(() => [RegistrationRequest])
+  allRegistrationRequests() {
+    return RegistrationRequest.find({
+      relations: ["municipality", "businessField"],
+    });
+  }
+
+  @Mutation(() => RegistrationRequest)
+  async createRegistrationRequest(
+    @Arg("data")
+    {
+      lastName,
+      firstName,
+      email,
+      businessID,
+      businessFieldID,
+      municipalityID,
+    }: RegistrationRequestInput
+  ): Promise<RegistrationRequest | undefined> {
+    const businessField = await BusinessField.findOne(businessFieldID);
+    if (!businessField) return undefined;
+
+    const municipality = await Municipality.findOne(municipalityID);
+    if (!municipality) return undefined;
+
+    const newRequest = await RegistrationRequest.create({
+      lastName,
+      firstName,
+      email,
+      businessID,
+      businessField,
+      municipality,
+    }).save();
+    console.log("new request created", newRequest);
+
+    return newRequest;
+  }
+}
