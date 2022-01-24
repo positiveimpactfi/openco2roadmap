77c484b5267f5149d62fa019b8327478131bc5cd
Yusif Salam-zade <yusalam@gmail.com>
2022-01-24 11:30:51 +0200

    fix (client): fix announcement bug when there are no announcements

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -5,6 +5,7 @@ import LoadingSpinner from "components/LoadingSpinner";
 import { sidebarLinks } from "data/links/sidebarLinks";
 import { useMeQuery } from "graphql/queries/users/me.generated";
 import useTranslation from "next-translate/useTranslation";
+import { Announcement } from "types/Annoucement";
 import { User } from "types/generatedTypes";
 
 const Home = () => {
@@ -19,14 +20,17 @@ const Home = () => {
 
 const UserLoggedIn: React.FC<{ user: Partial<User> }> = ({ user }) => {
   const { t } = useTranslation("common");
-  const announcements = JSON.parse(localStorage.getItem("announcements"));
+  const announcements: Announcement[] = JSON.parse(
+    localStorage.getItem("announcements")
+  );
   const latestAnnouncement = localStorage.getItem("latestAnnouncement");
 
   return (
     <>
-      {(!latestAnnouncement || latestAnnouncement !== announcements[0].id) && (
-        <Banner announcement={announcements[0]} />
-      )}
+      {announcements.length > 0 &&
+        (!latestAnnouncement || latestAnnouncement !== announcements[0].id) && (
+          <Banner announcement={announcements[0]} />
+        )}
       <div className="px-4 sm:px-6 lg:px-8 pb-4">
         <h2 className="text-xl mb-4">{`${t("home:greeting")}, ${
           user.firstName ?? user.email
