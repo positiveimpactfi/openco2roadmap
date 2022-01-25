22f47fcb31843ee8e75df5e694918da708f508fd
Yusif Salam-zade <yusalam@gmail.com>
2022-01-25 09:36:42 +0200

    feat (client): remove unnecessary pages in calculator
    Removes sites, emission sources and emission factors pages from calculator section

--- web/src/data/links/calculatorLinks.ts
+++ web/src/data/links/calculatorLinks.ts
@@ -3,23 +3,6 @@ import { linksWithDefaultIcons } from "utils/linkWithDefaultIcon";
 
 export const calculatorLinks: PageLink[] = linksWithDefaultIcons([
   {
-    name: "sites",
-    description:
-      "Hiilijalanjälkilaskentaan tarvittavat kulutustiedot liittyvät aina konkreettisesti johonkin yrityksesi toimipaikkaan. Tällä sivulla näet yrityksesi hiilijalanjälkilaskennassa käytetyn toimipaikkarakenteen.",
-    href: "/calculator/sites",
-  },
-  {
-    name: "emission_sources",
-    description: "Tutki mitä päästölähteitä yrityksesi laskentaan sisältyy.",
-    href: "/calculator/emission-sources",
-  },
-  {
-    name: "emission_factors",
-    description:
-      "Tutki mitä päästökertoimia yrityksesi laskentaan on käytettävissä.",
-    href: "/calculator/emission-factors",
-  },
-  {
     name: "consumption_data",
     description:
       "Hiilijalanjäljen laskeminen perustuu yrityksesi todellisiin kulutustietoihin. Saat nämä mm. energiayhtiöiltä, ajopäiväkirjoista ja kirjanpidosta. sivun kautta syötät energian, jätteiden, hankintojen ja hallinnon eri kulutustiedot, kuukausi- tai vuositasolla.",
