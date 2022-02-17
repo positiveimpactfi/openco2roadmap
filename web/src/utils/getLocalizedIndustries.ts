26bbcbbc9442a7b430410eb2c2f9fd347cb5538c
Yusif Salam-zade <yusalam@gmail.com>
2022-02-17 13:17:07 +0200

    enhancement (client): hide industry code

--- web/src/utils/getLocalizedIndustries.ts
+++ web/src/utils/getLocalizedIndustries.ts
@@ -5,7 +5,7 @@ export const localizedIndustries = (lang: "fi" | "en") => {
     return {
       ...i,
       id: i.code,
-      name: i.names[lang] + " (" + i.code + ")",
+      name: i.names[lang],
       children: i.subIndustries.map((s) => {
         return { ...s, id: s.code, name: s.names[lang] + " (" + s.code + ")" };
       }),
