a3a8461426b68afb72f7a273fe6dc204047c504b
Yusif Salam-zade <yusalam@gmail.com>
2022-02-23 14:22:18 +0200

    enhancement (client): remove KPI placeholder from organization settings page

--- web/src/pages/settings/organization-settings.tsx
+++ web/src/pages/settings/organization-settings.tsx
@@ -2,7 +2,6 @@ import { withAuth } from "components/Auth";
 import ClientOnly from "components/ClientsOnly";
 import EditOrganizationForm from "components/Forms/Organization/EditOrganizationForm";
 import SettingsPanel from "components/SettingsPanel";
-import { SectionUnderConstruction } from "components/UnderConstruction";
 import { useUser } from "hooks/useUser";
 import { NextPage } from "next";
 import { Translate } from "next-translate";
@@ -17,9 +16,6 @@ const OrganizationSettingsPage: NextPage = () => {
     >
       <ClientOnly>
         <CompanyInfo t={t} />
-      </ClientOnly>
-      <ClientOnly>
-        <CompanyKPIs t={t} />
       </ClientOnly>
     </SettingsPanel>
   );
@@ -39,24 +35,6 @@ const CompanyInfo: React.FC<{ t: Translate }> = ({ t }) => {
       </p>
       <div className="max-w-2xl">
         <EditOrganizationForm org={org} />
-      </div>
-    </div>
-  );
-};
-
-const CompanyKPIs: React.FC<{ t: Translate }> = ({ t }) => {
-  const { user } = useUser();
-
-  if (!user) return null;
-
-  return (
-    <div className="mt-4">
-      <h2 className="text-xl">{t("pages.org_settings.kpis.title")}</h2>
-      <p className="mb-4 text-sm text-gray-500">
-        {t("pages.org_settings.kpis.description")}
-      </p>
-      <div className="max-w-2xl">
-        <SectionUnderConstruction />
       </div>
     </div>
   );
