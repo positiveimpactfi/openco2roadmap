511583c0edacb993165be71a1ceae7ba77a970d7
Yusif Salam-zade <yusalam@gmail.com>
2022-02-26 16:34:22 +0200

    wip (server): mutation to update KPI

--- server/src/resolvers/kpi.ts
+++ server/src/resolvers/kpi.ts
@@ -76,4 +76,57 @@ export class KPIResolver {
     const savedKpi = await kpiBase.save();
     return savedKpi;
   }
+
+  @Authorized([Role.ADMIN, Role.COMPANY_ADMIN])
+  @Mutation(() => KPI)
+  async updateKPI(
+    @Ctx() { req }: MyContext,
+    @Arg("id") id: string,
+    @Arg("name", { nullable: true }) name: string,
+    @Arg("measurementUnit", () => MeasurementUnitType, { nullable: true })
+    measurementUnit: MeasurementUnitType
+  ): Promise<KPI | undefined> {
+    const user = await User.findOne(req.session.userId);
+    if (!user) {
+      return undefined;
+    }
+    const kpi = await KPI.findOne(id);
+    if (!kpi) {
+      req.log.error({ error: "no KPI found for id" }, "no KPI");
+      return undefined;
+    }
+    if (kpi.organization.id !== user.organizations[0].id) {
+      req.log.error(user, "access denied");
+      return undefined;
+    }
+    let propertiesChanged = {
+      name: false,
+      unit: false,
+    };
+    if (name) {
+      kpi.name = name;
+      propertiesChanged.name = true;
+    }
+    if (measurementUnit) {
+      const unit = await MeasurementUnit.findOne(measurementUnit);
+      if (unit) {
+        kpi.unit = unit;
+        propertiesChanged.unit = true;
+      }
+    }
+    if (propertiesChanged.name && propertiesChanged.unit) {
+      req.log.info(kpi, "Changed name and unit");
+      const savedKPI = await kpi.save();
+      return savedKPI;
+    } else if (propertiesChanged.name) {
+      req.log.info(kpi, "Changed KPI name");
+      const savedKPI = await kpi.save();
+      return savedKPI;
+    } else if (propertiesChanged.unit) {
+      req.log.info(kpi, "Changed KPI unit");
+      const savedKPI = await kpi.save();
+      return savedKPI;
+    }
+    return undefined;
+  }
 }
