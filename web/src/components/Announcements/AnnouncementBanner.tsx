5bc7468b9ae7ce1f48b28ba81b30338ae5db173d
Yusif Salam-zade <yusalam@gmail.com>
2022-03-02 10:42:19 +0200

    enhancement (client): announcement banner styling

--- web/src/components/Announcements/AnnouncementBanner.tsx
+++ web/src/components/Announcements/AnnouncementBanner.tsx
@@ -21,8 +21,8 @@ const Banner: React.FC<{ announcement: Announcement }> = ({ announcement }) => {
   return (
     <>
       {visible && (
-        <div className="relative bg-teal-600">
-          <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
+        <div className="relative rounded-md bg-teal-600">
+          <div className="mx-auto mb-2 max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
             <div className="pr-16 sm:px-16 sm:text-center">
               <p className="font-medium text-white">
                 <span>{localization.title}</span>
