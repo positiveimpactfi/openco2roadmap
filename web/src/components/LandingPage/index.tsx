5aeeff1d16a7dd464c798f6281d264428944c4f4
Yusif Salam-zade <yusalam@gmail.com>
2022-03-15 12:01:48 +0200

    feat (client): add certificate reader box to landing page

--- web/src/components/LandingPage/index.tsx
+++ web/src/components/LandingPage/index.tsx
@@ -16,27 +16,56 @@ export default function LandingPage() {
 
 const Hero = () => {
   return (
-    <main className="flex w-full flex-1 flex-col justify-between text-center">
-      <div className="bg-teal-600">
-        <div className="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-          <div className="text-center">
-            <h1 className="mt-1 text-4xl font-extrabold text-white">
-              Matkailualan CO2-laskuri
-            </h1>
-            <p className="mx-auto mt-5 max-w-2xl text-lg text-white">
-              Haluatko tietää matkailuyrityksesi hiilijalanjäljen? Matkailualan
-              maksuttomalla CO2-laskurilla voit laskea yrityksesi
-              hiilijalanjäljen ja kartoittaa suurimmat päästölähteet. Tulosten
-              avulla voit suunnitella toimintaa entistä ilmastoystävällisemmäksi
-              ja viestiä ilmastotyöstä.
-            </p>
-          </div>
-          <div className="mt-4 flex flex-wrap items-center justify-center gap-4">
-            <Button href="/register" title="Rekisteröidy käyttäjäksi" />
-            <Button href="/login" title="Siirry kirjautumissivulle" />
-          </div>
-        </div>
+    <main className="flex w-full  justify-center bg-teal-600 text-center ">
+      <div className="flex w-full max-w-7xl flex-col items-center justify-between lg:flex-row">
+        <TravelCalculatorBox />
+        <SertificateReaderBox />
       </div>
     </main>
+  );
+};
+
+const TravelCalculatorBox = () => {
+  return (
+    <div className="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:ml-10 lg:px-8">
+      <div className="text-center">
+        <h1 className="mt-1 text-4xl font-extrabold text-white">
+          Matkailualan CO2-laskuri
+        </h1>
+        <p className="mx-auto mt-5 max-w-2xl text-lg text-white">
+          Haluatko tietää matkailuyrityksesi hiilijalanjäljen? Matkailualan
+          maksuttomalla CO2-laskurilla voit laskea yrityksesi hiilijalanjäljen
+          ja kartoittaa suurimmat päästölähteet. Tulosten avulla voit
+          suunnitella toimintaa entistä ilmastoystävällisemmäksi ja viestiä
+          ilmastotyöstä.
+        </p>
+      </div>
+      <div className="mt-4 flex flex-wrap items-center justify-center gap-4">
+        <Button href="/register" title="Rekisteröidy käyttäjäksi" />
+        <Button href="/login" title="Siirry kirjautumissivulle" />
+      </div>
+    </div>
+  );
+};
+
+const SertificateReaderBox = () => {
+  return (
+    <div className="mx-auto mt-4 mb-8 flex max-w-sm flex-col items-center justify-center rounded-md bg-teal-900 px-4 pt-12 pb-8 sm:px-6 lg:mr-10 lg:h-96 lg:px-8">
+      <div className="text-center">
+        <h2 className="text-2xl font-extrabold text-white">
+          Sertifikaattilukija
+        </h2>
+        <p className="mt-4 text-white">
+          Sertifikaattilukija kokoaa matkailualan yritysten erilaisten
+          sertifikaattien tiedot yhteen paikkaan.
+        </p>
+      </div>
+      <div className="align-center mt-16 flex justify-center">
+        <Button
+          href="https://certreader.visifinland.fi"
+          title="Sirry lukijaan"
+        />
+      </div>
+    </div>
   );
 };
